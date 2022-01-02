005f236104d0218016e20d8fd9f8b1d812f6cec7
ja1dan <ajaidan0@gmail.com>
2022-01-02 15:50:42 -0500

    fix: patch documentation formatting issue

--- docs.py
+++ docs.py
@@ -1,17 +1,21 @@
 # shitty doc generator
 
 # imports
-import canisterpy
+import src.canisterpy.canister as canisterpy
 import inspect
 
 
+def normalize(tn: str): return tn.replace('        ', '    ')
+
+
 def doc(td: object):
-    bs = "\\"
     if inspect.isclass(td):
         print(f'# {td.__name__} (class):  {td.__doc__}')
         for method in [method for method in dir(td) if method.startswith('__') is False]:
             func = getattr(td, method)
-            print(f'# {func.__name__} ({type(func).__name__} of {td.__name__}):  {str(func.__doc__)}')
+            print(f'# {func.__name__} ({"`asynchronous`" if inspect.iscoroutinefunction(func) else "`synchronous`"} {type(func).__name__} of {td.__name__}):  {normalize(str(func.__doc__))}')
+    elif inspect.isfunction(td):
+        print(f'# {td.__name__} ({"`asynchronous`" if inspect.iscoroutinefunction(td) else "`synchronous`"} {type(td).__name__}):  {normalize(str(td.__doc__))}')
     else:
         print(f'# {td.__name__} ({type(td).__name__}):  {str(td.__doc__)}')
 
